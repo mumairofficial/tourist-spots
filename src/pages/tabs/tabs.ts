75e10d3dfcf93d0a329f242b56c1a274d38e3ba3
mumair <mumairofficial@gmail.com>
2017-05-17 01:41:37 +0500

    tabs automatically update according to loggedin user

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,36 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
 import { HomePage } from '../home/home';
+import { DashboardPage } from '../dashboard/dashboard';
+import { PlacesPage } from '../places/places';
+import { ProfilePage } from '../profile/profile';
+
+import { AuthProvider } from '../../providers/auth/auth';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
+  public isAdmin: boolean;
+  public isUser: boolean;
+
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = ProfilePage;
+  tab3Root = DashboardPage;
+  tab4Root = PlacesPage;
 
-  constructor() {
+  constructor(private _auth: AuthProvider) {
 
+    // TODO: auth service which tell wether loggedin user is admin or normal user
+    this.watchUserLoggedInState();
+
+  }
+
+  private watchUserLoggedInState(): void {
+    this._auth.watchIsLoggedIn().subscribe(authStatus => {
+      console.log("subscribe to watch dog: ", authStatus);
+      this.isAdmin = authStatus;
+    })
   }
 }
