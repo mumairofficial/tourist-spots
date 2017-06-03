2bda7d0267fb11097b0998fd5c0942281375c6dc
mumair <mumairofficial@gmail.com>
2017-06-04 01:11:21 +0500

    place details page intermediate comit

--- src/pages/place-detail/place-detail.ts
+++ src/pages/place-detail/place-detail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { AngularFireOfflineDatabase } from 'angularfire2-offline/database';
+import firebase from "firebase";
 import { Place } from '../../model/place';
 
 @Component({
@@ -10,14 +11,18 @@ import { Place } from '../../model/place';
 export class PlaceDetail {
 
   public place: Place;
+  public usersList: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afoDb: AngularFireOfflineDatabase) {
     this.place = this.navParams.data.selected_place;
+    console.log(this.place);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PlaceDetail');
-
   }
 
+  toNumber(val: string): number {
+    return Number(val);
+  }  
 }
