130872f52d4467cd3912e5f3bfbfc00a86946419
mumair <mumairofficial@gmail.com>
2017-06-04 01:15:33 +0500

    google map api integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireOfflineModule } from 'angularfire2-offline';
-import { FIREBASE_CONFIG } from '../config/firebase-config';
+import { FIREBASE_CONFIG, GOOGLE_MAP_API_KEY } from '../config/firebase-config';
 
 import { MyApp } from './app.component';
 
@@ -29,6 +29,7 @@ import { TextAvatarDirective } from '../components/text-avatar/text-avatar';
 import { LoginPage } from "../pages/login-page/login-page";
 import { RegisterPage } from "../pages/register/register";
 
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,10 @@ import { RegisterPage } from "../pages/register/register";
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
-    AngularFireOfflineModule
+    AngularFireOfflineModule,
+    AgmCoreModule.forRoot({
+      apiKey: GOOGLE_MAP_API_KEY
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
