898c6c272889fb620f712f986e320cffdd0a6d70
mumair <mumairofficial@gmail.com>
2017-06-03 15:36:35 +0500

    tabs are limited to users only, admin setion is removed

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { HomePage } from '../home/home';
-import { DashboardPage } from '../dashboard/dashboard';
 import { PlacesPage } from '../places/places';
 import { ProfilePage } from '../profile/profile';
 
@@ -15,22 +13,11 @@ export class TabsPage {
   public isAdmin: boolean;
   public isUser: boolean;
 
-  tab1Root = HomePage;
+  tab1Root = PlacesPage;
   tab2Root = ProfilePage;
-  tab3Root = DashboardPage;
-  tab4Root = PlacesPage;
 
   constructor(private _auth: AuthProvider) {
 
-    // TODO: auth service which tell wether loggedin user is admin or normal user
-    this.watchUserLoggedInState();
-
   }
 
-  private watchUserLoggedInState(): void {
-    this._auth.watchIsLoggedIn().subscribe(authStatus => {
-      console.log("subscribe to watch dog: ", authStatus);
-      this.isAdmin = authStatus;
-    })
-  }
 }
