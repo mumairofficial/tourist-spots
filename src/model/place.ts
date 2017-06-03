366015d45db821512479b2d3d57deb0da393c005
mumair <mumairofficial@gmail.com>
2017-06-04 01:11:01 +0500

    place entity updated with more fields

--- src/model/place.ts
+++ src/model/place.ts
@@ -2,10 +2,18 @@ export class Place {
     private _name: string;
     private _lat: number;
     private _lon: number;
+    private _district: string;
+    private _country: string;
+    private _description: string;
 
-    constructor(private name: string, private lat: number, private lon: number) {
-        this._name = name;
-        this._lat = lat;
-        this._lon = lon;
-    }
+
+	constructor(name: string, lat: number, lon: number, district: string, country: string, description: string) {
+		this._name = name;
+		this._lat = lat;
+		this._lon = lon;
+		this._district = district;
+		this._country = country;
+		this._description = description;
+	}
+    
 }
